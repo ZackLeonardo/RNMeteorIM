25252ebb959a86e2b37f76a6f4bc7814786a1d7f
ZackLeonardo <maiz9088@icloud.com>
2017-07-12 11:26:15 +0800

    暂存

---
+++ src/components/LoadEarlier.js
@@ -0,0 +1,22 @@
+/**
+ * React Native IM App with DDP(Meteor)
+ * the Component which loading earlier messages
+ *
+ * @zack
+ */
+import React, { Component } from 'react';
+import {
+  View,
+  ViewPropTypes,
+  ListView,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+class LoadEarlier extends Component {
+  render(){
+    return (
+      <View>
+      </View>
+    );
+  }
+}
---
+++ src/containers/MessageContainer.js
@@ -0,0 +1,42 @@
+/**
+ * React Native IM App with DDP(Meteor)
+ * the container which shwo messages in ListView
+ *
+ * @zack
+ */
+import React, { Component } from 'react';
+import {
+  View,
+  ViewPropTypes,
+  ListView,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+class MessageContainer extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      
+    };
+  }
+
+  render(){
+    return (
+      <View>
+        <ListView
+          enableEmptySections = {true}
+          automaticallyAdjustContentInsets = {false}
+          initialListSize = {20}
+          pageSize = {20}
+
+          dataSource = {this.state.dataSource}
+
+          renderRow = {this.renderRow()}
+        />
+      </View>
+    );
+  }
+}
+
+module.exports = MessageContainer;
