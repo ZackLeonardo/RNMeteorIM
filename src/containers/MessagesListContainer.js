5b02ee471ad9ba9cca6ca97e18a2ca8faf58e25e
ZackLeonardo <maiz9088@icloud.com>
2017-07-24 13:06:41 +0800

    完成简单版MessagesListContainer，测试通过。

--- src/containers/MessagesListContainer.js
+++ src/containers/MessagesListContainer.js
@@ -8,34 +8,52 @@ import React, { Component } from 'react';
 import {
   View,
   ViewPropTypes,
+  TouchableOpacity,
+  Text,
 } from 'react-native';
 import PropTypes from 'prop-types';
+
+import MessagesList from '../components/MessagesList';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import * as actions from '../actions/messages';
+import messages from '../reducers/messages';
 
 class MessagesListContainer extends Component{
   constructor(props){
     super(props);
-
-    this.state = {
-
-    };
   }
 
   render(){
+    const { dispatch, messages, users, myId, addMessage } = this.props;
     return (
-      <View>
-        <ListView
-          enableEmptySections = {true}
-          automaticallyAdjustContentInsets = {false}
-          initialListSize = {20}
-          pageSize = {20}
-
-          dataSource = {this.state.dataSource}
-
-          renderRow = {this.renderRow()}
+      <View >
+      <TouchableOpacity
+        style={styles.thisView}
+        onPress={() => addMessage({'2' : {id: '2', text: 'IndiaRRR', userId: '1', createdAt: '1995-12-25 10:02:00',sent: true}})}
+        accessibilityTraits = "text">
+        <Text> push me </Text>
+      </TouchableOpacity>
+        <MessagesList
+          messages={messages}
+          users = {users}
+          myId = {myId}
         />
+
       </View>
     );
   }
 }
 
-module.exports = MessageContainer;
+const styles = {
+  thisView: {
+    marginTop: 10,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#c5c5c5',
+  },
+}
+
+module.exports = MessagesListContainer;
--- src/containers/app.js
+++ src/containers/app.js
@@ -12,25 +12,25 @@ import {
     Text,
     TouchableOpacity,
 } from 'react-native';
-import { connect } from 'react-redux';
-import { addMessage, deleteMessage } from '../actions/messages';
 
-import Day from '../components/Day';
-import Avatar from '../components/Avatar';
-import Time from '../components/Time';
-import MessageText from '../components/MessageText';
-import MessageImage from '../components/MessageImage';
-import Bubble from '../components/Bubble';
-import Message from '../components/Message';
-import Meteor, { createContainer } from 'react-native-meteor';
+
+// import Day from '../components/Day';
+// import Avatar from '../components/Avatar';
+// import Time from '../components/Time';
+// import MessageText from '../components/MessageText';
+// import MessageImage from '../components/MessageImage';
+// import Bubble from '../components/Bubble';
+// import Message from '../components/Message';
+// import Meteor, { createContainer } from 'react-native-meteor';
 import MessagesList from '../components/MessagesList';
 
+import { connect } from 'react-redux';
 import * as actions from '../actions/messages';
-
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import messages from '../reducers/messages';
+import MessagesListContainer from './MessagesListContainer';
 
 const store = createStore(messages);
 
@@ -40,62 +40,6 @@ const App = () => {
       <MyApp />
     </Provider>
   );
-}
-
-
-
-class MessagesListContainer extends Component {
-
-  render(){
-    const { dispatch, messages, users, myId, addMessage } = this.props;
-    return (
-      <View style={styles.thisView}>
-      <TouchableOpacity
-        style={styles.thisView}
-        onPress={() => addMessage({'2' : {id: '2', text: 'IndiaRRR', userId: '1', createdAt: '1995-12-25 10:02:00',sent: true}})}
-        accessibilityTraits = "text">
-        <Text> push me </Text>
-      </TouchableOpacity>
-        <MessagesList
-          messages={messages}
-          users = {users}
-          myId = {myId}
-        />
-
-      </View>
-    );
-  }
-
-}
-
-const styles = {
-  left: StyleSheet.create({
-    container: {
-      marginTop: 20,
-      marginBottom: 10,
-      flex: 1
-    },
-    textStyle: {
-      color: 'red',
-    }
-  }),
-  right: StyleSheet.create({
-    container: {
-      marginTop: 20,
-      marginBottom: 10,
-      flex: 1
-    },
-    textStyle: {
-      color: 'red',
-    }
-  }),
-  thisView: {
-    marginTop: 10,
-  },
-  button: {
-    padding: 10,
-    backgroundColor: '#c5c5c5',
-  },
 }
 
 const MyApp = connect(
